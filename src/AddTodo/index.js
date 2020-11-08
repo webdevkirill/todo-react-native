04e5beadc8d7072b965fc4317ea6f64d3c78a898
webdevkirill <kirill-fjodorov00000@yandex.ru>
2020-11-09 00:19:26 +0300

    Добавляет вывод ошибки при пустой задаче

--- src/AddTodo/index.js
+++ src/AddTodo/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, TextInput, Button } from 'react-native';
+import { View, StyleSheet, TextInput, Button, Alert } from 'react-native';
 
 export const AddTodo = ({onSubmit}) => {
 
@@ -10,7 +10,7 @@ export const AddTodo = ({onSubmit}) => {
             onSubmit(value.trim());
             setValue('');
         } else {
-            //error
+            Alert.alert('Название задачи не может быть пустым')
         }
     }
 
