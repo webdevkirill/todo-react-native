75a67e8910292f53daf844a4ee494f3b18c3ad8e
webdevkirill <kirill-fjodorov00000@yandex.ru>
2020-11-09 00:17:26 +0300

    Добавляет обработку вводимых значений в инпут и добавляет правильноую задачу

--- src/AddTodo/index.js
+++ src/AddTodo/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, TextInput, Button } from 'react-native';
 
 export const AddTodo = ({onSubmit}) => {
 
+    const [value, setValue] = useState('');
+
+    const addTodoHandler = () => {
+        if (value.trim()) {
+            onSubmit(value.trim());
+            setValue('');
+        } else {
+            //error
+        }
+    }
+
     return (
         <View style={styles.block}>
-            <TextInput style={styles.input} />
+            <TextInput 
+                style={styles.input}
+                onChangeText={setValue}
+                value={value}
+                placeholder='Введите название задачи...'
+            />
             <Button 
-            title='Добавить' 
-            onPress={() => onSubmit('name')} />
+                title='Добавить' 
+                onPress={addTodoHandler}
+            />
         </View>
     )
 }
