f2b8d425ddf60f86bf45c1653aa1f08b42e53c39
webdevkirill <kirill-fjodorov00000@yandex.ru>
2020-11-08 23:15:03 +0300

    Добавляет статику для блока добавления todo айтема

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AddTodo } from './src/AddTodo';
 import { Navbar } from './src/Navbar';
 
 export default function App() {
     return (
-        <View style={styles.container}>
+        <View>
             <Navbar title='TodoApp' />
-            <StatusBar />
+            <View style={styles.container}>
+                <AddTodo />
+                <StatusBar />
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-    
+        paddingHorizontal: 20,
+        paddingVertical: 30
     },
 });
---
+++ src/AddTodo/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, StyleSheet, TextInput, Button } from 'react-native';
+
+export const AddTodo = () => {
+
+    return (
+        <View style={styles.block}>
+            <TextInput style={styles.input} />
+            <Button title='Добавить' />
+        </View>
+    )
+}
+
+export const styles = StyleSheet.create({
+    block: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    input: {
+        width: '70%',
+        padding: 10,
+        borderStyle: 'solid',
+        borderBottomWidth: 2,
+        borderBottomColor: '#3949ab'
+    }
+});
