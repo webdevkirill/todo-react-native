49da696df16abb6cbc2a828710d2b75c6cf25471
webdevkirill <kirill-fjodorov00000@yandex.ru>
2020-11-14 23:18:31 +0300

    Добавляет подключение шрифтов с помощью expo-font

--- App.js
+++ App.js
@@ -4,6 +4,14 @@ import { StyleSheet, View, Alert } from 'react-native';
 import { Navbar } from './src/components/Navbar';
 import { MainScreen } from './src/screens/MainScreen';
 import { TodoScreen } from './src/screens/TodoScreen';
+import * as Font from 'expo-font';
+
+const loadApp = async () => {
+    await Font.loadAsync({
+        'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
+        'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf')
+    })
+}
 
 export default function App() {
     const [todos, setTodos] = useState([
