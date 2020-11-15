a00c60273471ef521c78091b7db2f63fda9945c0
webdevkirill <kirill-fjodorov00000@yandex.ru>
2020-11-15 19:52:01 +0300

    Рефакторит код редюсера к объекту

--- src/context/todo/todoReducer.js
+++ src/context/todo/todoReducer.js
@@ -1,26 +1,32 @@
 import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from '../types';
 
-export const todoReducer = (state, action) => {
+const handlers = {
+    [ADD_TODO]: (state, {title}) => ({
+        ...state, 
+        todos: [
+            ...state.todos, 
+            {
+                id: `${Date.now().toString()}_${title}`,
+                title
+            }
+        ]
+    }), 
+    [REMOVE_TODO]: (state, {id}) => ({
+        ...state, todos: state.todos.filter((todo) => todo.id !== id)
+    }), 
+    [UPDATE_TODO]: (state, {id, title}) => ({
+        ...state, 
+        todos: [...state.todos].map((todo) => {
+            if (todo.id === id) {
+                todo.title = title
+            }
+            return todo
+        })
+    }),
+    DEFAULT: state => state
+}
 
-    switch (action.type) {
-        case ADD_TODO:
-            return {...state, todos: [
-                ...state.todos, 
-                {
-                    id: `${Date.now().toString()}_${action.title}`,
-                    title: action.title
-                }
-            ]}
-        case REMOVE_TODO:
-            return {...state, todos: state.todos.filter((todo) => todo.id !== action.id)}
-        case UPDATE_TODO:
-            return {...state, todos: [...state.todos].map((todo) => {
-                if (todo.id === action.id) {
-                    todo.title = action.title
-                }
-                return todo
-            })}
-        default:
-            return state
-    }
+export const todoReducer = (state, action) => {
+    const handler = handlers[action.type] || handlers.DEFAULT;
+    return handler(state, action)
 }
